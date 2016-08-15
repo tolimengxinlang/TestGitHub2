a69704377def5fd467331e63e57172a010cc4007
tolimengxinlang <tolimengxinlang@sina.com>
2016-08-15 20:32:25 +0800

    update

--- src/com/atsxsoft/test/GitHub.java
+++ src/com/atsxsoft/test/GitHub.java
@@ -4,7 +4,6 @@ public class GitHub {
 	public static void main(String[] args) {
 		System.out.println("hello git hub");
 		System.out.println("first row");
-		System.out.println("second row");
-		System.out.println("third row");
+
 	}
 }
