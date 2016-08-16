3045d880dbbbc996f8e226723253cf14dd898f23
tolimengxinlang <tolimengxinlang@sina.com>
2016-08-16 20:33:37 +0800

    update

--- src/com/atsxsoft/test/TestGitHub.java
+++ src/com/atsxsoft/test/TestGitHub.java
@@ -4,7 +4,6 @@ public class TestGitHub {
 	public static void main(String[] args) {
 		System.out.println("first row");
 		System.out.println("second row");
-		System.out.println("third row");
-		System.out.println("fourth row");
+
 	}
 }
